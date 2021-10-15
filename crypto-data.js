8e198a930affbbcd87fd7db21ef762dcd7133b9e
Harrison South <hsouth95@hotmail.co.uk>
2021-10-15 16:18:32 +0100

    Seperate out into html/css and js. Fix issue with misconfigured cryptos and missing data from api

---
+++ crypto-data.js
@@ -0,0 +1,7 @@
+const CRYPTO_DATA = [
+    { "id": "bitcoin", "name": "Bitcoin", "symbol": "BTC" },
+    { "id": "ethereum", "name": "Ethereum", "symbol": "ETH" },
+    { "id": "binancecoin", "name": "Binance Coin", "symbol": "BNB" },
+    { "id": "cardano", "name": "Cardano", "symbol": "ADA" },
+    { "id": "dogecoin", "name": "Dogecoin", "symbol": "DOGE" },
+];
