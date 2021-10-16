127fe75cd9ef87b5350c8bacf067bfb292eeb554
Harrison South <hsouth95@hotmail.co.uk>
2021-10-16 04:04:25 +0100

    Add more coins to help test

--- crypto-data.js
+++ crypto-data.js
@@ -4,4 +4,8 @@ const CRYPTO_DATA = [
     { "id": "binancecoin", "name": "Binance Coin", "symbol": "BNB" },
     { "id": "cardano", "name": "Cardano", "symbol": "ADA" },
     { "id": "dogecoin", "name": "Dogecoin", "symbol": "DOGE" },
+    { "id": "xrp", "name": "XRP", "symbol": "XRP" },
+    { "id": "solana", "name": "Solana", "symbol": "SOL" },
+    { "id": "polkadot", "name": "Polkadot", "symbol": "DOT" },
+    { "id": "pancakeswap-token", "name": "PancakeSwap", "symbol": "CAKE" },
 ];
